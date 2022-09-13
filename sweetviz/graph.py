89d8f50542d8195a8c4b94b95029f69b95ab6f39
hjanes123 <108413296+hjanes123@users.noreply.github.com>
2022-09-13 15:26:21 +0100

    Update graph.py

--- sweetviz/graph.py
+++ sweetviz/graph.py
@@ -17,8 +17,8 @@ register_matplotlib_converters()
 plt.rcParams['svg.fonttype'] = 'path'
 # plt.rcParams['svg.fonttype'] = 'none'
 
-COLOR_TARGET_SOURCE = "#a43e50"
-COLOR_TARGET_COMPARE = "#e998a6"
+COLOR_TARGET_SOURCE = "#ec555c"
+COLOR_TARGET_COMPARE = "#f5aaad"
 
 class Graph:
     def __init__(self):
